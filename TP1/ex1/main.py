5f9e61e8e0eba1bdc10d27d73632fba3b7a3fe53
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2022-10-21 12:14:30 +0100

    modified:   TP1/ex1/main.py

--- TP1/ex1/main.py
+++ TP1/ex1/main.py
@@ -16,20 +16,29 @@ class ex1:
 
     def processesPerTimeSpan(self, pessoas, timeSpan):
         processos = self.filtraProcessos(pessoas)
-        print(len(processos), len(pessoas))
-        ano = re.match("(([0-9]{1,4})-*)", processos[0][1]).group(2)
-        print(self.filtraPorAno(pessoas, ano))
-        # filtro por ano
+        processosPorData = {}
+        while (processos != []):
+            #print(len(processos), len(processosPorData))
+            ano = re.match("(([0-9]{1,4})-*)", processos[0][1]).group(2)
+            processosNoAno, processosNoutrosAnos = self.filtraPorAno(
+                processos, ano)
+            processosPorData[ano] = processosNoAno
+            processos = processosNoutrosAnos
+        print(processosPorData)
 
-    def filtraPorAno(self, pessoas, yearToFilter):
-        procPerYear = []
-        for pessoa in pessoas:
+    def filtraPorAno(self, processos, yearToFilter):
+        procPerYear = 0
+        processosNoutrosAnos = []
+        for pessoa in processos:
+            flag = 0
             for elem in pessoa:
-                print(elem)
-                # testar a passagem pelo formatador
-                if re.search(f"{yearToFilter}-[0-9]{2}-[0-9]{1,2}", elem):
-                    procPerYear.append(pessoa)
-        return procPerYear
+                if re.search(yearToFilter+r"-[0-9]{2}-[0-9]{1,2}", elem):
+                    flag = 1
+            if flag == 1:
+                procPerYear += 1
+            else:
+                processosNoutrosAnos.append(pessoa)
+        return (procPerYear, processosNoutrosAnos)
 
     def filtraProcessos(self, pessoas):
         processos = []
