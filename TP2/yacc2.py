f587ab65756b9768d082d2e4e30e2c3af78e5021
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2022-12-30 13:43:41 -0800

    yacc update

--- TP2/yacc2.py
+++ TP2/yacc2.py
@@ -56,7 +56,7 @@ def p_decl_NoAssign(p):
         parser.variaveis[varName] = None
         parser.vars += 1
     else:
-        print(f"Variável {varName} já definida.")
+        parser.error=(f"Variável {varName} já definida.")
         parser.exito = False
 
 
@@ -71,7 +71,7 @@ def p_decl_Int(p):
         parser.variaveis[varName] = value
         parser.vars += 1
     else:
-        print(f"Variável {varName} já definida.")
+        parser.error=(f"Variável {varName} já definida.")
         parser.exito = False
 
 # Criar variável com valor de outra var
@@ -86,11 +86,11 @@ def p_decl_Var(p):
             parser.variaveis[varName] = parser.variaveis[varValue]
             parser.vars += 1
         else:
-            print(f"Variável {varValue} não definida anteriormente")
+            parser.error = (f"Variável {varValue} não definida anteriormente")
             parser.exito=False
 
     else:
-        print(f"Variável {varName} já definida.")
+        parser.error = (f"Variável {varName} já definida.")
         parser.exito = False
 
 
@@ -105,7 +105,7 @@ def p_decl_op(p):
         parser.variaveis[varName] = varValue
         parser.vars += 1
     else:
-        print(f"Variável {varName} já definida.")
+        parser.error = (f"Variável {varName} já definida.")
         parser.exito = False
 
 
@@ -120,7 +120,7 @@ def p_decl_Lista_NoSize(p):
         parser.variaveis[listName] = []
         parser.vars += 1
     else:
-        print(f"Variável com o nome {listName} já definida anteriormente.")
+        parser.error = (f"Variável com o nome {listName} já definida anteriormente.")
         parser.exito = False
 
 
@@ -135,7 +135,7 @@ def p_decl_Lista(p):
         parser.variaveis[listName] = [None for i in range(size)]
         parser.vars += 1
     else:
-        print(f"Variável com o nome {listName} já definida anteriormente.")
+        parser.error = (f"Variável com o nome {listName} já definida anteriormente.")
         parser.exito = False
 
 
@@ -149,7 +149,7 @@ def p_decl_Matriz_NoSize(p):
         parser.variaveis[matrizName] = [[]]
         parser.vars += 1
     else:
-        print(f"Matriz {matrizName} já definida.")
+        parser.error = (f"Matriz {matrizName} já definida.")
         parser.exito = False
 
 
@@ -165,7 +165,7 @@ def p_decl_Matriz(p):
         parser.variaveis[matrizName] = [[None for j in range(size1)] for i in range(size2)]
         parser.vars += 1
     else:
-        print(f"Matriz {matrizName} já definida.")
+        parser.error = (f"Matriz {matrizName} já definida.")
         parser.exito = False
 
 
@@ -179,7 +179,7 @@ def p_decl_alteraVar_Int(p):
     if varName in parser.variaveis:
         parser.variaveis[varName] = int(value)
     else:
-        print(f"Variável {varName} não definida anteriormente.")
+        parser.error = (f"Variável {varName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -193,7 +193,7 @@ def p_decl_alteraVar_op(p):
     if varName in parser.variaveis:
         parser.variaveis[varName] = int(value)
     else:
-        print(f"Variável {varName} não definida anteriormente.")
+        parser.error = (f"Variável {varName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -208,10 +208,10 @@ def p_decl_alteraVar_Var(p):
         if varValue in parser.variaveis:
             parser.variaveis[varName] = parser.variaveis[varValue]
         else:
-            print(f"Variavel {varValue} não definida anteriormente")
+            parser.error = (f"Variavel {varValue} não definida anteriormente")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente.")
+        parser.error = (f"Variável {varName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -230,10 +230,10 @@ def p_decl_alteraLista_Int(p):
         if indexForReplacement<len(parser.variaveis[arrayName]):
             parser.variaveis[arrayName][indexForReplacement] = valueForReplacement
         else:
-            print(f"Indice fora de alcance")
+            parser.error = (f"Indice fora de alcance")
             parser.exito = False
     else:
-        print(f"Variável {arrayName} não definida anteriormente.")
+        parser.error = (f"Variável {arrayName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -249,10 +249,10 @@ def p_decl_alteraLista_op(p):
         if indexForReplacement<len(parser.variaveis[arrayName]):
             parser.variaveis[arrayName][indexForReplacement] = valueForReplacement
         else:
-            print(f"Indice fora de alcance")
+            parser.error = (f"Indice fora de alcance")
             parser.exito = False
     else:
-        print(f"Variável {arrayName} não definida anteriormente.")
+        parser.error = (f"Variável {arrayName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -269,13 +269,13 @@ def p_decl_alteraLista_Var(p):
             if indexForReplacement<len(parser.variaveis[arrayName]):
                 parser.variaveis[arrayName][indexForReplacement]= parser.variaveis[valueForReplacement]
             else:
-                print(f"Indice fora de alcance")
+                parser.error = (f"Indice fora de alcance")
                 parser.exito = False
         else:
-            print(f"Variável {valueForReplacement} não definida anteriormente.")
+            parser.error = (f"Variável {valueForReplacement} não definida anteriormente.")
             parser.exito = False
     else:
-        print(f"Variável {arrayName} não definida anteriormente.")
+        parser.error = (f"Variável {arrayName} não definida anteriormente.")
         parser.exito = False
 
 
@@ -319,10 +319,10 @@ def p_soma_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(p[1]) + parser.variaveis[varName]
         else:
-            print(f"Operação impossível: INT + NoneType{varName}")
+            parser.error = (f"Operação impossível: INT + NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 
@@ -344,10 +344,10 @@ def p_sub_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(p[1]) - parser.variaveis[varName]
         else:
-            print(f"Operação impossível: INT - NoneType{varName}")
+            parser.error = (f"Operação impossível: INT - NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 
@@ -369,10 +369,10 @@ def p_mult_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(p[1]) * parser.variaveis[varName]
         else:
-            print(f"Operação impossível: INT * NoneType{varName}")
+            parser.error = (f"Operação impossível: INT * NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 
@@ -394,10 +394,10 @@ def p_div_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(p[1]) / parser.variaveis[varName]
         else:
-            print(f"Operação impossível: INT / NoneType{varName}")
+            parser.error = (f"Operação impossível: INT / NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 
@@ -419,10 +419,10 @@ def p_exp_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(math.pow(int(p[1]),parser.variaveis[varName]))
         else:
-            print(f"Operação impossível: INT ^ NoneType{varName}")
+            parser.error = (f"Operação impossível: INT ^ NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 
@@ -444,10 +444,10 @@ def p_resto_id(p):
         if parser.variaveis[varName]!=None:
             p[0] = int(int(p[1]) % parser.variaveis[varName])
         else:
-            print(f"Operação impossível: INT % NoneType{varName}")
+            parser.error = (f"Operação impossível: INT % NoneType{varName}")
             parser.exito=False
     else:
-        print(f"Variável {varName} não definida anteriormente")
+        parser.error = (f"Variável {varName} não definida anteriormente")
         parser.exito = False
 
 #
@@ -461,12 +461,12 @@ def p_Busca_Lista(p):
     matrizName = p[2]
     indice1 = int(p[3])
     if isinstance(parser.variaveis[matrizName], int) or parser.variaveis[matrizName]==None:
-        print("Operação inválida sobre o tipo de dados")
+        parser.error = ("Operação inválida sobre o tipo de dados")
         parser.exito = False
     elif indice1 < len(parser.variaveis[matrizName]) :
         p[0] = parser.variaveis[matrizName][indice1]
     else:
-        print(f"Indice fora de alcance")
+        parser.error = (f"Indice fora de alcance")
         parser.exito = False
 
 
@@ -479,12 +479,12 @@ def p_Busca_Matriz(p):
     indice1 = int(p[3])
     indice2 = int(p[4])
     if isinstance(parser.variaveis[matrizName], int) or parser.variaveis[matrizName]==None:
-        print("Operação inválida sobre o tipo de dados")
+        parser.error = ("Operação inválida sobre o tipo de dados")
         parser.exito = False
     elif indice1 < len(parser.variaveis[matrizName]) and indice2 < len(parser.variaveis[matrizName][indice1]):
         p[0] = parser.variaveis[matrizName][indice1][indice2]
     else:
-        print(f"Indice fora de alcance")
+        parser.error = (f"Indice fora de alcance")
         parser.exito = False
 
 
@@ -503,35 +503,35 @@ def p_maisGrande(p):
     maisGrande : MAISGRANDE ABREPC INT VIRG INT FECHAPC
     '''
     p[0] = f"{int(p[3])>int(p[5])}"
-    print(p[0])
+    parser.error = (p[0])
 
 def p_maisPiqueno(p):
     '''
     maisPiqueno : MAISPIQUENO ABREPC INT VIRG INT FECHAPC
     '''
     p[0] = f"{int(p[3])<int(p[5])}"
-    print(p[0])
+    parser.error = (p[0])
 
 def p_gemeo(p):
     '''
     gemeo : GEMEO ABREPC INT VIRG INT FECHAPC
     '''
     p[0] = f"{int(p[3])==int(p[5])}"
-    print(p[0])
+    parser.error = (p[0])
 
 def p_maisGrandeOuGemeo(p):
     '''
     maisGrandeOuGemeo : MAISGRANDEOUGEMEO ABREPC INT VIRG INT FECHAPC
     '''
     p[0] = f"{int(p[3])>=int(p[5])}"
-    print(p[0])
+    parser.error = (p[0])
 
 def p_maisPiquenoOuGemeo(p):
     '''
     maisPiquenoOuGemeo : MAISPIQUENOOUGEMEO ABREPC INT VIRG INT FECHAPC
     '''
     p[0] = f"{int(p[3])<=int(p[5])}"
-    print(p[0])
+    parser.error = (p[0])
 
 
 def p_ExprLog(p):
@@ -558,41 +558,111 @@ def p_ie_1Var(p):
         p[0]=f"{int(p[1]) and parser.variaveis[var]}"
         print(p[0])
     else:
-        print(f"Variável {var} não definida")
+        parser.error = (f"Variável {var} não definida")
         parser.exito = False
 
 def p_ie_1Var1(p):
     '''
     ie : ID IE INT
     '''
-    var = p[3]
+    var = p[1]
     if var in parser.variaveis: 
-        p[0]=f"{int(p[1]) and parser.variaveis[var]}"
+        p[0]=f"{parser.variaveis[var] and int(p[3])}"
         print(p[0])
     else:
-        print(f"Variável {var} não definida")
+        parser.error = (f"Variável {var} não definida")
         parser.exito = False
+
+def p_ie_2Var(p):
+    '''
+    ie : ID IE ID
+    '''
+    var = p[1]
+    var2 = p[3]
+    if var in parser.variaveis: 
+        if var2 in parser.variaveis:
+            p[0]=f"{parser.variaveis[var] and parser.variaveis[var2]}"
+            print(p[0])
+        else:
+            parser.error = (f"Variável {var2} não definida")
+    else:
+        parser.error = (f"Variável {var} não definida")
+        parser.exito = False
+
 
 def p_oue(p):
     '''
     oue : INT OUE INT
     '''
     p[0]=f"{int(p[1]) or int(p[3])}"
+    print(p[0])
+
+def p_oue_1Var(p):
+    '''
+    oue : INT OUE ID
+    '''
+    var = p[3]
+    if var in parser.variaveis: 
+        p[0]=f"{int(p[1]) or parser.variaveis[var]}"
+        print(p[0])
+    else:
+        parser.error = (f"Variável {var} não definida")
+        parser.exito = False
+
+def p_oue_1Var1(p):
+    '''
+    oue : ID OUE INT
+    '''
+    var = p[1]
+    if var in parser.variaveis: 
+        p[0]=f"{parser.variaveis[var] or int(p[3])}"
+        print(p[0])
+    else:
+        parser.error = (f"Variável {var} não definida")
+        parser.exito = False
+
+def p_oue_2Var(p):
+    '''
+    oue : ID OUE ID
+    '''
+    var = p[1]
+    var2 = p[3]
+    if var in parser.variaveis: 
+        if var2 in parser.variaveis:
+            p[0]=f"{parser.variaveis[var] or parser.variaveis[var2]}"
+            print(p[0])
+        else:
+            parser.error = (f"Variável {var2} não definida")
+    else:
+        parser.error = (f"Variável {var} não definida")
+        parser.exito = False
 
 def p_noum(p):
     '''
-    noum : noum INT
+    noum : NOUM ABREPC INT FECHAPC
     '''
     p[0]=f"{not int(p[3])}"
 
+def p_noum_var(p):
+    '''
+    noum : NOUM ABREPC ID FECHAPC
+    '''
+    var = p[2]
+    if var in parser.variaveis:
+        p[0]= not parser.variaveis[var]
+        print(p[0])
+    else:
+        parser.error = (f"Variável {var} não definida")
+        parser.exito = False
+
+
 
 def p_error(p):
-    print(p)
     try:
         syntatic_sugar(p.value)
         parser.exito = False
     except:
-        print(p)
+        parser.error = (p)
         parser.exito=False
 
 
